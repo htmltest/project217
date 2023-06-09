bbd33ad6a2a75676cffb8c9b592e54a996968493
htrmltest <struev.vs@gmail.com>
2023-06-09 13:11:40 +0300

    smart captcha

--- js/tools.js
+++ js/tools.js
@@ -36,38 +36,101 @@ function initForm(curForm) {
         $(this).val($(this).val()).change();
     });
 
+    curForm.find('.captcha-container').each(function() {
+        if ($('script#smartCaptchaScript').length == 0) {
+            $('body').append('<script src="https://captcha-api.yandex.ru/captcha.js?render=onload&onload=smartCaptchaLoad" defer id="smartCaptchaScript"></script>');
+        } else {
+            if (window.smartCaptcha) {
+                var curID = window.smartCaptcha.render(this, {
+                    sitekey: smartCaptchaKey,
+                    callback: smartCaptchaCallback,
+                    invisible: true,
+                    hideShield: true,
+                    hl: 'en'
+                });
+                $(this).attr('data-smartid', curID);
+            }
+        }
+    });
+
     curForm.validate({
         ignore: '',
         submitHandler: function(form) {
             var curForm = $(form);
-            if (curForm.hasClass('ajax-form')) {
-                curForm.addClass('loading');
-                var formData = new FormData(form);
+            var smartCaptchaWaiting = false;
+            curForm.find('.captcha-container').each(function() {
+                if (curForm.attr('form-smartcaptchawaiting') != 'true') {
+                    var curBlock = $(this);
+                    var curInput = curBlock.find('input[name="smart-token"]');
+                    curInput.removeAttr('value');
+                    smartCaptchaWaiting = true;
+                    $('form[form-smartcaptchawaiting]').removeAttr('form-smartcaptchawaiting');
+                    curForm.attr('form-smartcaptchawaiting', 'false');
 
-                $.ajax({
-                    type: 'POST',
-                    url: curForm.attr('action'),
-                    processData: false,
-                    contentType: false,
-                    dataType: 'json',
-                    data: formData,
-                    cache: false
-                }).fail(function(jqXHR, textStatus, errorThrown) {
-                    curForm.find('.message').remove();
-                    curForm.append('<div class="message message-error"><div class="message-text">Сервис временно недоступен, попробуйте позже.</div></div>')
-                    curForm.removeClass('loading');
-                }).done(function(data) {
-                    curForm.find('.message').remove();
-                    if (data.status) {
-                        curForm.html('<div class="message message-success"><div class="message-text">' + data.message + '</div></div>')
-                    } else {
-                        curForm.append('<div class="message message-error"><div class="message-text">' + data.message + '</div></div>')
+                    if (!window.smartCaptcha) {
+                        alert('Сервис временно недоступен, попробуйте позже.');
+                        return;
                     }
-                    curForm.removeClass('loading');
-                });
-            } else {
-                form.submit();
+                    var curID = $(this).attr('data-smartid');
+                    window.smartCaptcha.execute(curID);
+                } else {
+                    curForm.removeAttr('form-smartcaptchawaiting');
+                }
+            });
+
+            if (!smartCaptchaWaiting) {
+
+                if (curForm.hasClass('ajax-form')) {
+                    curForm.addClass('loading');
+                    var formData = new FormData(form);
+
+                    $.ajax({
+                        type: 'POST',
+                        url: curForm.attr('action'),
+                        processData: false,
+                        contentType: false,
+                        dataType: 'json',
+                        data: formData,
+                        cache: false
+                    }).fail(function(jqXHR, textStatus, errorThrown) {
+                        curForm.find('.message').remove();
+                        curForm.append('<div class="message message-error"><div class="message-text">Сервис временно недоступен, попробуйте позже.</div></div>')
+                        curForm.removeClass('loading');
+                    }).done(function(data) {
+                        curForm.find('.message').remove();
+                        if (data.status) {
+                            curForm.html('<div class="message message-success"><div class="message-text">' + data.message + '</div></div>')
+                        } else {
+                            curForm.append('<div class="message message-error"><div class="message-text">' + data.message + '</div></div>')
+                        }
+                        curForm.removeClass('loading');
+                    });
+                } else {
+                    form.submit();
+                }
             }
         }
     });
+}
+
+var smartCaptchaKey = 'uahGSHTKJqjaJ0ezlhjrbOYH4OxS6zzL9CZ47OgY';
+
+function smartCaptchaLoad() {
+    $('.captcha-container').each(function() {
+        if (!window.smartCaptcha) {
+            return;
+        }
+        var curID = window.smartCaptcha.render(this, {
+            sitekey: smartCaptchaKey,
+            callback: smartCaptchaCallback,
+            invisible: true,
+            hideShield: true
+        });
+        $(this).attr('data-smartid', curID);
+    });
+}
+
+function smartCaptchaCallback(token) {
+    $('form[form-smartcaptchawaiting]').attr('form-smartcaptchawaiting', 'true');
+    $('form[form-smartcaptchawaiting] [type="submit"]').trigger('click');
 }
